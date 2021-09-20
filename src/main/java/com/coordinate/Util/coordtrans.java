189875c75052395967c05322d55b2fe15dd38a0b
1090414202 <1090414202@qq.com>
2021-09-20 23:47:58 +0800

    例子测试完成

--- src/main/java/com/coordinate/Util/coordtrans.java
+++ src/main/java/com/coordinate/Util/coordtrans.java
@@ -1,5 +1,25 @@
 package com.coordinate.Util;
 
+import org.osgeo.proj4j.CRSFactory;
+import org.osgeo.proj4j.CoordinateReferenceSystem;
+import org.osgeo.proj4j.CoordinateTransform;
+import org.osgeo.proj4j.CoordinateTransformFactory;
+
 public class coordtrans {
+    public CoordinateTransform coordtrans() {
+
+        CRSFactory targetFactory = new CRSFactory();
+        CRSFactory crsFactory = new CRSFactory();
+        //目标坐标系统
+        String target_param =  "+proj=longlat +datum=WGS84 +no_defs ";
+        CoordinateReferenceSystem target = targetFactory.createFromParameters("wgs84", target_param);
+        //源坐标系统
+        String xian80_param = "+proj=longlat +a=6378140 +b=6356755.288157528 +towgs84=115.8,-154.4,-82.3,0,0,0,8 +no_defs ";
+        CoordinateReferenceSystem xian80 = crsFactory.createFromParameters("xian80", xian80_param);
+
+        CoordinateTransformFactory ctf = new CoordinateTransformFactory();
+        CoordinateTransform transform = ctf.createTransform(xian80, target);
+        return transform;
+    }
 
 }
---
+++ src/test/java/test.java
@@ -0,0 +1,18 @@
+import com.coordinate.Util.coordtrans;
+import org.junit.Test;
+import org.osgeo.proj4j.CoordinateTransform;
+import org.osgeo.proj4j.ProjCoordinate;
+
+public class test {
+    @Test
+    public void test() {
+        coordtrans test = new coordtrans();
+        ProjCoordinate projCoordinate = new ProjCoordinate(121.976469682, 41.4470288037);
+        test.coordtrans().transform(projCoordinate, projCoordinate);
+        Double dNorth = projCoordinate.y;
+        Double dEast = projCoordinate.x;
+        System.out.print(dNorth);
+        System.out.print("/");
+        System.out.print(dEast);
+    }
+}
